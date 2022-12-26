454a8ea31ba40faa627bcb18844f00d7e18064f4
TranHuyHiep <tranhuyhiep0710@gmail.com>
2022-12-26 23:27:06 +0700

    done login

--- login/login.js
+++ login/login.js
@@ -1,25 +1,36 @@
+const ACCESS_TOKEN = 'token';
+const ID_USER = 'idUser';
+
 function login() {
-    var user=$("#username").val();
-    var pass=$("#password").val();
-
-    let data = {
-        user: user,
-        password: pass
+    const username = $("#username").val();
+    const password = $("#password").val();
+    const Account = {
+        username: username,
+        password: password
     }
-
-    if(user!="" && pass!="") {
-        $.ajax
-        ({
-            type: 'post',
-            url: '',
-            data: JSON.stringify(data),
-            success: function (response) {
-                if (response == "success") {
-                    window.location.href = "../index.html"
-                } else {
-                    alert("Wrong Details");
-                }
+    const RESPONSE_FAIL = 'Fail'
+    $.ajax({
+        type: 'POST',
+        url: 'http://localhost:8081/account/login',
+        data: JSON.stringify(Account),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        success: (token) => {
+            console.log(token)
+            if (token.token !== RESPONSE_FAIL) {
+                localStorage.setItem(ACCESS_TOKEN, token.token);
+                localStorage.setItem(ID_USER, token.userId);
+                alert("Đăng nhập thành công")
+                window.location.href = "../index.html"
             }
-        });
-    }
+        },
+        error: function (token) {
+            console.log(token)
+            const notification = `<p style="color: #e05353">Sai tài khoản hoặc mật khẩu</p>`;
+            $('#body-notification').html(notification);
+            const div_notification = $('#notification');
+            const toast = new bootstrap.Toast(div_notification)
+        }
+    })
 }
