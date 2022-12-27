725f8f9ca291109bf03abeeb63d9098772c846a7
JulyHa <nguyenthingu3107@gmail.com>
2022-12-27 09:29:20 +0700

    'post'

--- profile/profile.js
+++ profile/profile.js
@@ -18,10 +18,11 @@ function LoadMoreToggle(){
 }
 
 function getAllPostByUser(){
-    let id = localStorage.getItem("iduser");
+    let id = localStorage.getItem("idUser");
+    console.log(id)
     $.ajax({
         type: 'get',
-        url: 'http://localhost:8081/posts',
+        url: 'http://localhost:8081/posts/profile/' + id,
         success: function (response) {
             console.log(response)
             let html = "";
@@ -29,21 +30,17 @@ function getAllPostByUser(){
                 html += `<div class="status-field-container write-post-container">
                         <div class="user-profile-box">
                             <div class="user-profile">
-                                <img src="../images/${item.appUser.image}" alt="">
+                                <img src="../images/${item.appUser.image}" alt="Ảnh đại diện">
                                 <div>
-                                    <p>${item.appUser.account.username}</p>
+                                    <p>${item.appUser.displayName}</p>
                                     <small>${item.time}</small>
                                 </div>
                             </div>
-<!--                            <div>-->
-<!--                                <a href="#"><i class="fas fa-ellipsis-v"></i></a>-->
-<!--                            </div>-->
                         </div>
                         <div class="status-field">
                             <p>
                                 ${item.content}
                             </p>
-<!--                            <img src="../images/feed-image-1.png" alt="">-->
 
                         </div>
                         <div class="post-reaction">
@@ -52,9 +49,6 @@ function getAllPostByUser(){
                                 <div><img src="../images/comments.png" alt="">${item.cmtCount}</div>
                                 <div><img src="../images/share.png" alt="">100</div>
                             </div>
-<!--                            <div class="post-profile-picture">-->
-<!--                                <img src="../images/profile-pic.png " alt=""> <i class=" fas fa-caret-down"></i>-->
-<!--                            </div>-->
                         </div>
                     </div>`
             })
@@ -65,25 +59,51 @@ function getAllPostByUser(){
 }
 
 function loadInfo(){
-    let id = localStorage.getItem("iduser");
+    let id = localStorage.getItem("idUser");
     $.ajax({
         type: 'get',
-        url: 'http://localhost:8081/user/1',
+        url: 'http://localhost:8081/user/'+id,
         success: function (response) {
-            console.log("../images/" + response.bgimage);
-            let html = `<img src="../images/${response.bgimage}" className="coverImage" alt="Ảnh bìa">`
-            // let html = `../images/${response.bgimage}`
-            $("#background").src(html)
-            $("#profile").src = "../images/" + response.image
+            $("#background").attr("src","../images/" + response.bgimage)
+            $(".profile").attr("src","../images/" + response.image)
+            $(".nameProfile").html(response.displayName)
+            $("#about-me").html(response.aboutMe)
+            $("#phone").html(response.phoneNumber)
+            $("#live").html(response.address)
+            $("#email").html(response.email)
         }
     })
 }
+function postStatus(){
+    let id = localStorage.getItem("idUser")
+    alert("Click")
+    let data = {
+        content: document.getElementById("content"),
+        time:"2022-09-22 23:11:28",
+        cmtCount: 0,
+        likeCount: 0
+    }
+    $.ajax({
+        type: 'Post',
+        url: "http://localhost:8081/posts",
+        data: JSON.stringify(data),
+        success: function (response){
+            alert("Đăng bài thành công!")
+            alert(response)
+        },
+        error:function (err){
+            console.log(err)
+        }
+
+    })
+}
 function likePost(){
+    alert("like");
     $.ajax({
         type: 'post',
         url:'http://localhost:8081/like/1/1',
     })
 }
 
-// loadInfo()
+loadInfo()
 getAllPostByUser()
